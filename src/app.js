9e2f60094db214780ca956fe809d90481faecaa4
Aayushshah <shahayush688@gmail.com>
2024-11-05 15:52:54 +0530

    ApiResponse is added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
-import express from 'express '
+import express from 'express'
 import cors from 'cors '
-
+import cookieParser from 'cookie-parser'
 export const app = express()
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
-    
+
 }))
+
+app.use(express.json({limit:"20kbs"}))
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true,limit:"20kbs"}))
+app.use(cookieParser())
+
 
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import {connectDB} from './db/index.js'
 import dotenv from 'dotenv'
-
+import {app} from './app.js'
 dotenv.config({
     path:'./.env'
 })
 connectDB()
 .then(()=>{
-
+    app.listen(process.env.PORT)
+    console.log(`server side port is connected to ${process.env.PORT}`)
 })
 .catch((error)=>{
     console.log(`connection error to port ${process.env.PORT}`,error)
